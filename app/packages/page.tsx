07a7492b41df5f72974a15b7e2d430722ba558db
Umair Amjad <omair@nxthatch.com>
2026-08-25 21:46:20 +0500

    Add Faqs on the Pakage page

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,6 +1,7 @@
 import PricingPlans from "@/components/packages/PricingPlans";
 import WhatsIncluded from "@/components/packages/WhatsIncluded";
 import FAQ from "@/components/shared/FAQ";
+import FinalCTA from "@/components/shared/FinalCTA";
 
 export default function PackagesPage() {
   return (
@@ -8,6 +9,7 @@ export default function PackagesPage() {
       <PricingPlans />
       <WhatsIncluded />
       <FAQ />
+      <FinalCTA />
     </main>
   );
 }
