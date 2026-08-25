1212db66daa061ac4e29ff8715bd36b056332347
Umair Amjad <omair@nxthatch.com>
2026-08-25 22:10:27 +0500

    Add Contact Page

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import ContactForm from "@/components/contact/ContactForm";
+import FAQ from "@/components/shared/FAQ";
+import FinalCTA from "@/components/shared/FinalCTA";
+
+export default function ContactPage() {
+  return (
+    <main>
+      <ContactForm />
+      <FAQ />
+      <FinalCTA />
+    </main>
+  );
+}
